798094e1a7dc506dfa65156f15bed96ebe9ca53c
PiterValiente <78941196+PiterAngel@users.noreply.github.com>
2025-09-19 10:45:33 -0600

    subiendo datos extra

---
+++ front-back/backend/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import time
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/execute', methods=['POST'])
+def get_first_word():
+    data = request.get_json()
+    message = data.get('command', '')
+
+    # Dividir el mensaje en palabras
+    words = message.split()
+
+    if words:
+        message = f'[Success] => comando {words[0]} ejecutado exitosamente'
+    else:
+        message = "No se encontraron palabras en el mensaje."
+
+    respuesta = {
+        'estado': 'OK',
+        'mensaje': message,
+    }
+
+    # Esperamos 1 segundo, para simular proceso de ejecución
+    time.sleep(1)
+
+    return jsonify(respuesta)
+
+if __name__ == '__main__':
+    app.run(debug=True)
